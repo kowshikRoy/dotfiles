f144552e32c0903fbbb3f4e6505a466426b9ad8e
Repon Kumar Roy <repon.roy@hotmail.com>
2021-01-03 23:20:54 +0800

    add zsh autosuggestions

--- .config/dotfiles/scripts.sh
+++ .config/dotfiles/scripts.sh
@@ -1,8 +1,11 @@
-set -eux -o pipefail
+set -eux
 if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions ]; then  
         git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions; 
 fi
 
+if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then  
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
 if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/themes/powerlevel10k ]; then  
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
